f93b6ae1e21aa58f167db0caeb9b2b0d165e9f14
vino <vino0908@outlook.com>
2021-06-21 22:52:54 +0800

    feat: 支持拷贝数组

--- test/index.js
+++ test/index.js
@@ -43,10 +43,10 @@ describe('deepClone', () => {
             assert(a.child.name === b.child.name)
         });
         it('能够复制数组对象', () => {
-            const a = [1, [2, 3, 4]]
+            const a = [[1, 23], [2, 3, 4]]
             const b = deepClone(a)
             assert(a !== b)
-            assert(a[0] === b[0])
+            assert(a[0] !== b[0])
             assert(a[1] !== b[1])
             assert.deepEqual(a, b)
         });
