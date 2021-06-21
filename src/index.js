a86d0b7b5347887dcce3413de041228360b6da7d
vino <vino0908@outlook.com>
2021-06-21 22:42:56 +0800

    feat: 能够复制普通对象

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-function deepClone() {
+function deepClone(source) {
 
+    if (source instanceof Object) {
+        const dist = new Object()
+        for (let k in source) {
+            //递归
+            dist[k] = deepClone(source[k])
+        }
+        return dist
+    }
+    return source
 }
 
 module.exports = deepClone
--- test/index.js
+++ test/index.js
@@ -7,7 +7,40 @@ const assert = chai.assert
 const deepClone = require('../src/index')
 
 describe('deepClone', () => {
-    it('是一个函数', () => {
-        assert.isFunction(deepClone)
-    });
+    describe('1.基本', () => {
+        it('是一个函数', () => {
+            assert.isFunction(deepClone)
+        });
+        it('能够复制基本类型', () => {
+            const n = 123
+            const n2 = deepClone(n)
+            assert(n === n2)
+            const string = '12345'
+            const s2 = deepClone(string)
+            assert(string === s2)
+            const b = true
+            const b2 = deepClone(b)
+            assert(b === b2)
+            const null1 = null
+            const null2 = deepClone(null1)
+            assert(null1 === null2)
+            const u = undefined
+            const u2 = deepClone(u)
+            assert(u === u2)
+            const sym = Symbol()
+            const sym1 = deepClone(sym)
+            assert(sym === sym1)
+        });
+    })
+    describe('2.对象', () => {
+        it('能够复制普通对象', () => {
+            const a = {name: 'vino', age: 18, child: {name: 'vin'}}
+            const b = deepClone(a)
+            assert(a !== b)
+            assert(a.name === b.name)
+            assert(a.age === b.age)
+            assert(a.child !== b.child)
+            assert(a.child.name === b.child.name)
+        });
+    })
 })
